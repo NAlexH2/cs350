24af4c492d05ffeec12fc63a14dd8eb3cd2c444e
LazG7 <nharris@pdx.edu>
2022-05-07 21:30:28 -0700

    Finished HW5:q1:q2. Changes to W6D2 notes.

--- 0-notes/w6d2_depthalgo.py
+++ 0-notes/w6d2_depthalgo.py
@@ -2,10 +2,12 @@ from collections import deque
 
 def dfs(g, u, v):
     """
-    g = [[1,2],[0,4],[0,3,5],[2,4,8],[1,3,6],[2,6,7],[4,5,9],[5,8],[3,7,9],[6,8]]
-    dfs(g, 0, 9)
+    >>> g = [[1,2],[0,4],[0,3,5],[2,4,8],[1,3,6],[2,6,7],[4,5,9],[5,8],[3,7,9],[6,8]]
+    >>> dfs(g, 0, 9)
     """
-    return dfs_rec(g, u, v, {u}) #pass in u as type = set
+    p = dfs_rec(g, u, v, {u}) #pass in u as type = set
+    print(p)
+    return
         
 def dfs_rec(g, u, v, seen):
     if u == v:
@@ -13,26 +15,32 @@ def dfs_rec(g, u, v, seen):
     
     for w in g[u]:
         if w not in seen:
-            p = dfs(g, w, v, seen | {w}) #union w with set seen
+            p = dfs_rec(g, w, v, seen | {w}) #union w with set seen
             if not p is None: #if P != none
                 return [u]+p
              
     return None
 
-def bfs_rec(g, u, v):
+def bfs(g, u, v):
     parent = {u : u}
     queue = deque()
     queue.append(u)
-    while not queue.empty():
+    while not queue:
         w = queue.popleft()
         for x in g[w]:
             if x not in parent:
                 parent[x] = w
                 queue.append(x)
                 if x == v:
-                    return makepath(parent) #in your notes
-    
-    
+                    return makepath(parent)
+                
+def makepath(p, s, e):
+    path = [e]
+    while e != s:
+        e = p[e]
+        path.append(e)
+    return path.reverse()
+
 if __name__ == "__main__":        
     import doctest
     doctest.testmod()
